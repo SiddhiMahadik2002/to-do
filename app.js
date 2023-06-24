d4c790f8e861f96db9b41dd5d5931f8388613821
Siddhi <siddhimahadik257@gmail.com>
2023-06-24 11:54:37 +0530

    Added three fuctionalities

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-let todo = [];
+let inputBox = document.getElementById("input")
+let btn = document.getElementById("btn")
+let totalTodoCount = document.getElementById("totaltodos")
 
-function addTodo(todo){
-    todo.push(todo)
+let todoArray = []
+
+function addTodo(){
+    let userEnteredValue = inputBox.value;
+    todoArray.push(userEnteredValue);
+    console.log(todoArray)
+    calculateTodos()
+}
+btn.addEventListener("click",addTodo)
+
+function calculateTodos(){
+    let totalcount = todoArray.length;
+    totalTodoCount.innerText = totalcount;
+    console.log(totalcount)
 }
