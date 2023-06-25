d35050d5860910de4c772ab551ff300de5ca0224
Siddhi <siddhimahadik257@gmail.com>
2023-06-25 20:11:12 +0530

    Added displaytodos functionality

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
 let inputBox = document.getElementById("input")
 let btn = document.getElementById("btn")
 let totalTodoCount = document.getElementById("totaltodos")
+let displayTodosArea = document.getElementById("displayTodosArea")
 
 let todoArray = []
 
 function addTodo(){
     let userEnteredValue = inputBox.value;
-    todoArray.push(userEnteredValue);
-    console.log(todoArray)
-    calculateTodos()
+    if(userEnteredValue.length >= 1){  
+        todoArray.push(userEnteredValue);
+
+    calculateTodos();
+    displayTodos();
+     inputBox.value = "";
+    }
+    else{
+        alert("Please enter something")
+    }
 }
 btn.addEventListener("click",addTodo)
 
 function calculateTodos(){
     let totalcount = todoArray.length;
     totalTodoCount.innerText = totalcount;
-    console.log(totalcount)
+   
+}
+
+function displayTodos(){
+    let todos = ""
+    for(let i = 0; i < todoArray.length ;i++){
+        todos = todos + `
+        <div class="singletodo">
+                    <p>${ todoArray[i] }</p>
+                
+                    <button>
+                        <ion-icon name="trash"></ion-icon>
+                    </button>
+                    
+        </div>
+        `
+    }
+    displayTodosArea.innerHTML = todos;
+
 }
